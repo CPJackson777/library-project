bde50a7148c17c6f55b39b53ce223763ec1b1259
Charles Jackson <cpjackson572@gmail.com>
2020-02-09 22:22:50 -0600

    added more files and url paths

---
+++ libraryapp/urls.py
@@ -0,0 +1,11 @@
+#This file will define all of the URLs that your library application will respond to
+
+from django.urls import path
+from .views import *
+
+app_name = "libraryapp"
+
+urlpatterns = [
+    path('', book_list, name='home'),
+    path('books/', book_list, name='books'),
+]
